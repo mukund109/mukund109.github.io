f47397283afd6e3abb042dace44c82738a5dfd0a
mukund109 <mukund.chaudhry@gmail.com>
2022-08-23 01:27:59 +0530

    add another example

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const element = document.getElementById("number")
+const another = document.getElementById("another")
 
 function toggleOn() {
   element.innerHTML = "4394830"
@@ -8,5 +9,15 @@ function toggleOff() {
   element.innerHTML = "4.3 mil"
 }
 
+function toggleOnether() {
+  another.innerHTML = "4394830"
+}
+
+function toggleOffether() {
+  another.innerHTML = "4.3 million"
+}
+
 element.addEventListener('mouseover', toggleOn)
 element.addEventListener('mouseleave', toggleOff)
+another.addEventListener('mouseover', toggleOnether)
+another.addEventListener('mouseleave', toggleOffether)
